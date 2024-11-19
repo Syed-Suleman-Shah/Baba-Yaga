9949db081f27e0eb28dfafccccf0d777b300699c
RADI-SE <radi1y@hotmail.com>
2024-11-19 08:13:03 -0800

    chore: remove unnecessary dependencies and improve login functionality
    - Removed unused dependencies to optimize the project
    - Enhanced login functionality for better user experience

--- FrontEnd/src/components/Common/Navbar.jsx
+++ FrontEnd/src/components/Common/Navbar.jsx
@@ -1,58 +1,24 @@
-// import React from 'react'
-// import { FaSearch, FaShoppingCart, FaUser } from 'react-icons/fa'
-// import { Link } from 'react-router-dom'
-
-// const Navbar = () => {
-//   return (
-//     <nav className='bg=white shadow-md'>
-//         <div className='container mx-auto px-4:px-16 lg:px-24 py-4 flex justify-between items-center'>
-//             <div className='text-lg font-bold'>
-//                 <Link to="/">Khareed-Ghar</Link>
-//             </div>
-//             <div className='relative flex-1 mx-4'>
-//                 <form action="">
-//                     <input type="text" placeholder='Search' className='w-full border py-2 px-4' />
-//                     <FaSearch className='absolute top-3 right-3 text-red-500'></FaSearch>
-//                 </form>
-//             </div>
-//             <div className='flex items-center space-x-4'>
-//                 <Link path="/cart">
-//                     <FaShoppingCart className='text-lg' />
-//                 </Link>
-//                 <button className='hidden md:block'>
-//                     Login | Register
-//                 </button>
-//                 <button className='block md:hidden'>
-//                     <FaUser />
-//                 </button>
-//             </div>
-//         </div>
-//         <div className='flex items-center justify-center space-x-10 py-4 text-sm font-bold'>
-//             <Link className='hover:underline'> Home </Link>
-//             <Link className='hover:underline'> Cart </Link>
-//             <Link className='hover:underline'> Sell </Link>
-//             <Link className='hover:underline'> Categories </Link>
-//         </div>
-//     </nav>
-//   )
-// }
-
-// export default Navbar
-
-
-
 import React, { useState } from "react";
 import { FaSearch, FaShoppingCart, FaUser, FaXbox } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import categories from "../../../public/CategoryData.json"; // Adjust the path based on your file structure
+import { useAuthService } from "../../services/authService";
 
 const Navbar = () => {
+  const { signout } = useAuthService();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const handleLogin = () => {
+    window.location.href = '/signin';
+  }
+  const handleLogout = async () => {
+    await signout();
+    window.location.href = '/';
+  }
   return (
     <nav className="bg-white shadow-md">
       <div className="container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center">
@@ -73,8 +39,8 @@ const Navbar = () => {
           <Link to="/cart">
             <FaShoppingCart className="text-lg" />
           </Link>
-          <button className="hidden md:block">Login | Register</button>
-          <button className="hidden md:block">Logout</button>
+          <button className="hidden md:block" onClick={handleLogin}>Login | Register</button>
+          <button className="hidden md:block" onClick={handleLogout}>Logout</button>
           <button className="block md:hidden">
             <FaUser />
           </button>
